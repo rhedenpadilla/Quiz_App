f095de236bc18db80ca12285efe0f1a28722218c
Rheden Padilla <rhdnpadilla@gmail.com>
2026-08-16 15:02:09 +0800

    Add classes pages

---
+++ src/components/views/Classes.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Classes() {
+  return (
+    <div className='container' style={{ padding: '2rem' }}>
+      <h2>Classes</h2>
+      <p>Content coming soon...</p>
+    </div>
+  );
+}
+
